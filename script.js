c23010a86cb9deea426ae9e310247fbfc34b57be
LUIZ CODE <beat.luiz23@hotmail.com>
2024-08-12 14:10:43 -0300

    tt

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-
+const funcoes = [
+    document.getElementById().style.display="none",
+    () => console.log('Função 2'),
+    () => console.log('Função 3')
+];
 
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('div-content.html')
+    fetch('inicial.html')
         .then(response => response.text())
         .then(data => {
-            document.getElementById('importedDiv').innerHTML = data;
+            document.getElementById('inicial').innerHTML = data;
         })
         .catch(error => console.error('Erro ao carregar o conteúdo:', error));
 });
+
+
+
+    
+document.addEventListener("DOMContentLoaded", function format() {
+    fetch('format.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('format').innerHTML = data;
+        })
+        .catch(error => console.error('Erro ao carregar o conteúdo:', error));
+});
+
+
